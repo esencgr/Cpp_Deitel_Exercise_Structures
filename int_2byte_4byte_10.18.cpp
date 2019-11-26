04cbc243810965233e0a20cde5a88fa5f893c4a6
Cagri Esen <esencgr@gmail.com>
2019-11-26 15:39:09 +0300

    add new

--- int_2byte_4byte_10.18.cpp
+++ int_2byte_4byte_10.18.cpp
@@ -1,33 +1,33 @@
 #include <iostream>
 using namespace std;
 
-void display_4bits( unsigned );
-void display_2bits( unsigned );
+void display_4bytes( unsigned );
+void display_2bytes( unsigned );
 
 int main(){
 
     int number;
     cin >> number;
 
-    cout << "4 bits : ";
-    display_4bits( number );
+    cout << "4 bytes : ";
+    display_4bytes( number );
 
-    cout << "2 bits : ";
-    display_2bits( number );
+    cout << "2 bytes : ";
+    display_2bytes( number );
 
  return 0;
 }
 
 // this function is implemnting for using and working 2 bytes computer
-void display_2bits( unsigned value ){
+void display_2bytes( unsigned value ){
 
     const int SHIFT = ( 8 * sizeof( unsigned )/2 ) - 1;   // Alocated memory of unsigned variable ( 8*4/2 - 1 = 31 )
     const unsigned mask = 1 << SHIFT;                     // 10000000 00000000 00000000 00000000 -- 32 bit
 
     cout << value << " = ";
     for (unsigned c = 1; c <= SHIFT; c++){
-        cout << ( value & mask ? '1' : '0' );          // AND operators decides of which bits 1 or 0
-        value <<= 1;                                   // compares for each bit to 01000000 and shifted version each 8 bits uncouple.
+        cout << ( value & mask ? '1' : '0' );          // AND operators decides of which bites 1 or 0
+        value <<= 1;                                   // compares for each bit to 01000000 and shifted version each 8 bites uncouple.
         if (c % 8 == 0)
             cout << ( ' ' );
     }
@@ -36,7 +36,7 @@ void display_2bits( unsigned value ){
 }
 
 // this function is implemnting for using and working 4 bytes computer
-void display_4bits( unsigned value ){
+void display_4bytes( unsigned value ){
 
     // const int SHIFT = 8 * sizeof( unsigned ) - 1;    // Alocated memory of unsigned variable ( 8*4 - 1 = 31 ) ( 8 bit computer )
     // const unsigned mask = 1 << SHIFT;                // 10000000 00000000 00000000 00000000 -- 32 bit
@@ -44,8 +44,8 @@ void display_4bits( unsigned value ){
 
     cout << value << " = ";
     for (unsigned c = 1; c <= 32; c++){
-        cout << ( value & mask ? '1' : '0' );           // AND operators decides of which bits 1 or 0
-        value <<= 1;                                    // compares for each bit to 01000000 and shifted version each 8 bits uncouple.
+        cout << ( value & mask ? '1' : '0' );           // AND operators decides of which bites 1 or 0
+        value <<= 1;                                    // compares for each bit to 01000000 and shifted version each 8 bites uncouple.
         if (c % 8 == 0)
             cout << ( ' ' );
     }
